8849286f1bdd016aab880f7dc99ceeb5aa1b90f2
damien arnaud <darnaud@mail.com>
2019-02-11 16:57:20 +0100

    some slight changes

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -15,11 +15,11 @@ class GamesController < ApplicationController
     @start_time = params[:start_time]
     @end_time = Time.now
     @result = {}
-    # @result[:time] = @end_time - @start_time
-    @result[:time] = 2
+    # binding.pry
+    @result[:time] = @end_time - @start_time.to_time
     @executive_test = word_testeur(@word, @grid)
-    @executive_test["found"] ? @result[:score] = 1 / @result[:time] + @word.length.to_f : @result[:score] = 0
-    @executive_test["found"] ? @result[:message] = "Congratulations! '#{@word}.upcase' is a valid English word!" : @result[:message] = @executive_test["error"]
+    @executive_test["found"] ? @result[:score] = ((1 / @result[:time])*100).round / 100.0 + @word.length.to_f : @result[:score] = 0
+    @executive_test["found"] ? @result[:message] = "Congratulations! '#{@word.upcase}' is a valid English word!" : @result[:message] = @executive_test["error"]
   end
 
   def word_testeur(word, grid)
@@ -32,7 +32,7 @@ class GamesController < ApplicationController
       final_test = JSON.parse(open("https://wagon-dictionary.herokuapp.com/#{word}").read)
       final_test["error"] = "Sorry but '#{word.upcase}' does not seem to be a valid English word" if final_test["found"] == false
     else
-      final_test = { "found" => false, "word" => word, "error" => "Sorry but '#{word.upcase}' can be built out of #{grid}" }
+      final_test = { "found" => false, "word" => word, "error" => "Sorry but '#{word.upcase}' can be built out of the grid" }
     end
     return final_test
   end
